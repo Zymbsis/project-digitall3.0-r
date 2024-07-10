03b836c7975aade79eacf52fec04ec232c19c31b
Zymbsis-fs <Oleksandra-fsdev@gmail.com>
2024-07-10 22:51:48 +0200

    added folder-wrapper and component subtitle markup

--- src/shared/index.js
+++ src/shared/index.js
@@ -1,5 +1,6 @@
-export { default as Button } from './Button/Button.jsx';
-export { default as Container } from './Container/Container.jsx';
-export { default as Icon } from './Icon/Icon.jsx';
-export { default as Section } from './Section/Section.jsx';
-export { default as Title } from './Title/Title.jsx';
+export { default as Button } from './components/Button/Button.jsx';
+export { default as Container } from './components/Container/Container.jsx';
+export { default as Icon } from './components/Icon/Icon.jsx';
+export { default as Section } from './components/Section/Section.jsx';
+export { default as Title } from './components/Title/Title.jsx';
+export { default as Subtitle } from './components/Subtitle/Subtitle.jsx';
