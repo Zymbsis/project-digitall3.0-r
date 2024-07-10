a47bfb920b0256152e8a3084cf6a99e5fe64a920
Zymbsis-fs <Oleksandra-fsdev@gmail.com>
2024-07-10 23:45:43 +0200

    added persist

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
+import { createPortal } from 'react-dom';
 import { Section, Container } from 'shared';
 
 const App = () => {
   return (
     <Section>
-      <Container>Hello world</Container>
+      <Container>
+        <button
+          type="button"
+          onClick={() => {
+            return createPortal(
+              <div
+                style={{
+                  backgroundColor: 'red',
+                  width: '100%',
+                  height: '100%',
+                }}
+              >
+                Hello
+              </div>,
+              document.querySelector('#modal-root')
+            );
+          }}
+        >
+          Test Button
+        </button>
+      </Container>
     </Section>
   );
 };
