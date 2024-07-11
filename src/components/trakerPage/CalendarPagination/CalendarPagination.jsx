a0d06fbea65dca815c80f84870ccdeea68cd98f0
naowmi <vadimpelyushenko@gmail.com>
2024-07-11 16:06:24 +0200

    pagination v1

--- src/components/trakerPage/CalendarPagination/CalendarPagination.jsx
+++ src/components/trakerPage/CalendarPagination/CalendarPagination.jsx
@@ -1,7 +1,46 @@
-// import css from './CalendarPagination.module.css'
+import css from './CalendarPagination.module.css'
+import { Container } from 'shared';
+import { useEffect, useState } from "react";
 
 const CalendarPagination = () => {
-  return <div></div>;
+ const [currentDate, setCurrentDate] = useState(new Date());
+ const user = 
+
+useEffect(() => {
+  setCurrentDate(new Date()); //Set new date
+}, [])
+
+  const handlePreviouslyMonth = () => {
+    setCurrentDate((prevDate) => {
+      const prevMonthDate = new Date(prevDate.getFullYear(), prevDate.getMonth() - 1, 1);
+      return prevMonthDate;
+    });
+   };
+ 
+  const handleNextMonth = () => {
+    setCurrentDate((nextDate) => {
+      const nextMonthDate = new Date(nextDate.getFullYear(), nextDate.getMonth() + 1, 1);
+      return nextMonthDate;
+    });
+  }
+  const formatDate = (date) => {
+    const format = { month: 'long', year: 'numeric'};
+    const data = new Intl.DateTimeFormat('en-US', format).format(date)
+    const [month, year] = data.split(' ');
+    return `${month}, ${year}`
+  }
+  return <Container className={css.box}>
+    <h2 className={css.title}>Month</h2>
+    <div className={css.dateBox}>
+    <button onClick={handlePreviouslyMonth}>
+        Prev
+    </button>
+    <p className={css.date}>{formatDate(currentDate)}</p>
+    <button onClick={handleNextMonth}>
+      Next
+    </button>
+    </div>
+  </Container>;
 };
 
 export default CalendarPagination;
