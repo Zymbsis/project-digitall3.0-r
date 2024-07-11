a49654c0d8365dac2b2fe798eeafaae3bb82b991
Zymbsis-fs <Oleksandra-fsdev@gmail.com>
2024-07-11 10:48:00 +0200

    added Gilroy font

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,9 @@
-import { createPortal } from 'react-dom';
 import { Section, Container } from 'shared';
 
 const App = () => {
   return (
     <Section>
-      <Container>
-        <button
-          type="button"
-          onClick={() => {
-            return createPortal(
-              <div
-                style={{
-                  backgroundColor: 'red',
-                  width: '100%',
-                  height: '100%',
-                }}
-              >
-                Hello
-              </div>,
-              document.querySelector('#modal-root')
-            );
-          }}
-        >
-          Test Button
-        </button>
-      </Container>
+      <Container></Container>
     </Section>
   );
 };
