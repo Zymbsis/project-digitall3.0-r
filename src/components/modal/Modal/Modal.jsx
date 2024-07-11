d006f3f6e8e16f9e0b6dcb6d32aae237141b9a47
Zymbsis-fs <Oleksandra-fsdev@gmail.com>
2024-07-11 17:39:17 +0200

    .

--- src/components/modal/Modal/Modal.jsx
+++ src/components/modal/Modal/Modal.jsx
@@ -1,7 +1,9 @@
-// import css from './Modal.module.css'
+import css from './Modal.module.css';
 
 import { useCallback, useEffect } from 'react';
 import { useModal } from 'context';
+import { createPortal } from 'react-dom';
+import { Container, Icon } from 'shared';
 
 const Modal = ({ children }) => {
   const { setModalContent } = useModal();
@@ -19,7 +21,19 @@ const Modal = ({ children }) => {
     return () => window.removeEventListener('keydown', closeModal);
   }, [closeModal]);
 
-  return <div></div>;
+  return createPortal(
+    <div className={css.modalBackdrop} onClick={closeModal}>
+      <Container className={css.test}>
+        <div className={css.modalContainer}>
+          <button className={css.modalButtonClose} onClick={closeModal}>
+            <Icon iconId="icon-x" />
+          </button>
+          {children}
+        </div>
+      </Container>
+    </div>,
+    document.querySelector('#modal-root')
+  );
 };
 
 export default Modal;
