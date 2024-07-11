c06f29c14f03c4f25dea86736695f9c8c8efd014
naowmi <vadimpelyushenko@gmail.com>
2024-07-11 18:00:07 +0200

    v3

--- src/components/trakerPage/CalendarPagination/CalendarPagination.jsx
+++ src/components/trakerPage/CalendarPagination/CalendarPagination.jsx
@@ -2,23 +2,44 @@ import css from './CalendarPagination.module.css'
 import { Container } from 'shared';
 import { useEffect, useState } from "react";
 
+const fetchUser = async => {
+  return {
+    email: 'test@example.com',
+    password: '12345678',
+    name: 'Kate',
+    gender: 'woman',
+    avatar: 'image.png',
+    dailyNorma: 1.8,
+    weight: 0,
+    activeHours: 0,
+    createdAt: '2024-02-01T00:00:00.000Z',
+    updatedAt: '2024-07-01T00:00:00.000Z'
+  };
+}
 const CalendarPagination = () => {
- const [currentDate, setCurrentDate] = useState(new Date());
- const user = 
+  const [currentDate, setCurrentDate] = useState(new Date());
+  const [navDate, setNavDate] = useState(new Date());
+ const [user, setUser] = useState(null)
 
 useEffect(() => {
-  setCurrentDate(new Date()); //Set new date
-}, [])
+  const getUser = async () => {
+    const userData = await fetchUser();
+    setUser(userData);
+    setCurrentDate(new Date());
+    setNavDate(new Date());
+  };
+  getUser();
+}, []);
 
   const handlePreviouslyMonth = () => {
-    setCurrentDate((prevDate) => {
+    setNavDate((prevDate) => {
       const prevMonthDate = new Date(prevDate.getFullYear(), prevDate.getMonth() - 1, 1);
       return prevMonthDate;
     });
    };
  
   const handleNextMonth = () => {
-    setCurrentDate((nextDate) => {
+    setNavDate((nextDate) => {
       const nextMonthDate = new Date(nextDate.getFullYear(), nextDate.getMonth() + 1, 1);
       return nextMonthDate;
     });
@@ -29,13 +50,23 @@ useEffect(() => {
     const [month, year] = data.split(' ');
     return `${month}, ${year}`
   }
-  return <Container className={css.box}>
+  if (!user) {
+    return <div>User not found!</div>;
+  }
+  const userDate = new Date(user.createdAt);
+
+  const isPrevDis = navDate.getFullYear() < userDate.getFullYear() ||
+  (navDate.getFullYear() === userDate.getFullYear() && 
+  navDate.getMonth() <= userDate.getMonth());
+ 
+ 
+ return <Container className={css.box}>
     <h2 className={css.title}>Month</h2>
     <div className={css.dateBox}>
-    <button onClick={handlePreviouslyMonth}>
+    <button onClick={handlePreviouslyMonth} disabled={isPrevDis}>
         Prev
     </button>
-    <p className={css.date}>{formatDate(currentDate)}</p>
+    <p className={css.date}>{formatDate(navDate)}</p>
     <button onClick={handleNextMonth}>
       Next
     </button>
