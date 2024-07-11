5e996db493b9be5644758b1a7dea05465f3b0926
naowmi <vadimpelyushenko@gmail.com>
2024-07-11 16:06:45 +0200

    v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { Section, Container } from 'shared';
-
+import CalendarPagination from "../src/components/trakerPage/CalendarPagination/CalendarPagination";
 const App = () => {
   return (
     <Section>
       <Container>Hello world</Container>
+      <CalendarPagination/>
     </Section>
   );
 };
