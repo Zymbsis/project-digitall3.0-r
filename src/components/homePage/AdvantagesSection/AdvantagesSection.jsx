65e7a95980ccce3f6f576f7737345796c13af194
dan1delon <143418624+dan1delon@users.noreply.github.com>
2024-07-12 18:27:10 +0300

    upd

--- src/components/homePage/AdvantagesSection/AdvantagesSection.jsx
+++ src/components/homePage/AdvantagesSection/AdvantagesSection.jsx
@@ -3,9 +3,20 @@ import css from './AdvantagesSection.module.css';
 const AdvantagesSection = () => {
   return (
     <div className={css.wrapper}>
-      {/* <div className={css.customers}></div>
-      <div className={css.advantages}></div> */}
-      <img
+      <div className={css.customers}></div>
+      <div className={css.advantages}>
+        <div className={css.habit}>
+          <div className={css.dot}></div>
+          <p className={css.habitText}>Habit drive</p>
+        </div>
+        <div className={css.statistics}>
+          <p className={css.statisticsText}>View statistics</p>
+        </div>
+        <div className={css.rate}>
+          <p className={css.rateText}>Personal rate setting</p>
+        </div>
+      </div>
+      {/* <img
         srcSet="/img/HomePage/Customers-1x.png 1x, /img/HomePage/Customers-2x.png 2x"
         src="/img/HomePage/Customers-1x.png"
         alt="customers"
@@ -16,7 +27,7 @@ const AdvantagesSection = () => {
         src="/img/HomePage/Benefits-1x.png"
         alt="advantages"
         className={css.advantages}
-      />
+      /> */}
     </div>
   );
 };
