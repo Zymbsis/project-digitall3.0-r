3caabd437092828eb7c06e5d6e17fa3e126e3720
Kateryna1812 <super.katuysha2013@gmail.com>
2024-07-12 21:48:33 +0300

    Update DeleteWaterModal.jsx

--- src/components/modal/DeleteWaterModal/DeleteWaterModal.jsx
+++ src/components/modal/DeleteWaterModal/DeleteWaterModal.jsx
@@ -1,7 +1,35 @@
 // import css from './DeleteWaterModal.module.css'
 
 const DeleteWaterModal = () => {
+<<<<<<< Updated upstream
   return <div></div>;
+=======
+  const { closeModal } = useModal();
+
+  const handleDelete = () => {
+
+    dispatch(deleteWaterRecord());
+  };
+
+  const handleCancel = () => {
+    closeModal();
+  };
+
+  return (
+    <Modal onClose={handleCancel}>
+      <div className="modal-content">
+        <h2 className="title">Delete entry</h2>
+        <p>Are you sure you want to delete the entry?</p>
+        <Button className="button" type="button" onClick={handleDelete}>
+          Delete
+        </Button>
+        <Button className="button" type="button" onClick={handleCancel}>
+          Cancel
+        </Button>
+      </div>
+    </Modal>
+  );
+>>>>>>> Stashed changes
 };
 
 export default DeleteWaterModal;
