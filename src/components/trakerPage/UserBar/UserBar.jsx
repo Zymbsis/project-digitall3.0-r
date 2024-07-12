224f39be271868faeb684696303dbd607f14bb17
HalynaMal <142045196+HalynaMal@users.noreply.github.com>
2024-07-12 13:26:24 +0300

    check svg

--- src/components/trakerPage/UserBar/UserBar.jsx
+++ src/components/trakerPage/UserBar/UserBar.jsx
@@ -7,10 +7,7 @@ const UserBar = ({ userName, avatarUrl, openUserBarPopover }) => {
       <span className={css.userName}>{userName}</span>
       <img src={avatarUrl} alt="Avatar" className={css.avatar} />
       <svg className={css.chevronDown}><use href="./sprite.svg#icon-chevronDown"></use></svg>
-      {/* <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
-  <path d="M5 12.5L10 7.5L15 12.5" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-</svg> */}
-    </button>
+      </button>
   );
 };
 
--- src/components/trakerPage/UserBarPopover/UserBarPopover.jsx
+++ src/components/trakerPage/UserBarPopover/UserBarPopover.jsx
@@ -6,12 +6,12 @@ const UserBarPopover = ({ openUserSettingsModal, openLogOutModal }) => {
   return (
     <div className={css.userBarPopover}>
       <button className={css.settingsButton} onClick={openUserSettingsModal}>
-      <svg className={css.iconSetting}><use href="./sprite.svg#icon-settings"></use></svg>
+      <svg className={css.iconSetting}><use href="./icons/sprite.svg#icon-settings"></use></svg>
         <span className={css.btnSetting}>Setting</span>
       </button>
 
       <button className={css.logoutButton} onClick={openLogOutModal}>
-      <svg className={css.iconLogOut}><use href="./sprite.svg#icon-log-out" width='16' height='16'></use></svg>
+      <svg className={css.iconLogOut}><use href="./icons/sprite.svg#icon-log-out" width='16' height='16'></use></svg>
         <span className={css.logoutButton}>Log out</span>
       </button>
     </div>
