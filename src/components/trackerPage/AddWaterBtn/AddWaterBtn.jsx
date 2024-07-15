717caaa908921538c70bd96b682b6bef56335f39
AlbinaKost <143324469+AlbinaKost@users.noreply.github.com>
2024-07-15 15:09:00 +0300

    upd

--- src/components/trackerPage/AddWaterBtn/AddWaterBtn.jsx
+++ src/components/trackerPage/AddWaterBtn/AddWaterBtn.jsx
@@ -1,18 +1,47 @@
 import css from './AddWaterBtn.module.css'
-
+import Button from '../shared/Button/Button.jsx';
 // import React, { useState } from "react";
 
 
-const AddWaterBtn = ({ addWater }) => {
-  const handleClick = () => {
-    addWater();
-  };
+// const AddWaterBtn = ({ addWater }) => {
+//   const handleClick = () => {
+//     addWater();
+//   };
 
+//   return (
+//     <div className={css.container}>
+//       <button type="button" className={css.addWaterBtn} onClick={handleClick}>
+//         Add water
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default AddWaterBtn;
+const AddWaterBtn = ({
+  buttonClassName,
+  iconClassName,
+  spanClassName,
+  iconId,
+  iconWidth,
+  iconHeight,
+}) => {
   return (
-    <div className={css.container}>
-      <button type="button" className={css.addWaterBtn} onClick={handleClick}>
-        Add water
-      </button>
+    <div>
+      <Button
+        variant=".outline"
+        className={`${css.addWaterButton} ${buttonClassName}`}
+      >
+        <IconPlusWater
+          className={`${css.iconPlusWater} ${iconClassName}`}
+          iconId={iconId}
+          width={iconWidth}
+          height={iconHeight}
+        />
+        <span className={`${css.addWaterSpan} ${spanClassName}`}>
+          Add water
+        </span>
+      </Button>
     </div>
   );
 };
--- src/components/trackerPage/WaterItem/WaterItem.jsx
+++ src/components/trackerPage/WaterItem/WaterItem.jsx
@@ -7,20 +7,21 @@ import Icon from 'shared/components/Icon/Icon';
 const WaterItem = () => {
   return (
     <div className={css.waterItem}>
-          <Icon iconId="icon-eye" className={css.waterIconEye}
+           <Icon iconId="" className={css.waterIcon} />
       <div className={css.waterItemWrap}>
         <p className={css.waterItemMl}>250 ml</p>
         <p className={css.waterItemData}>data</p>
       </div>
       <div className={css.waterItemBtnWrap}>
         <button type="button" className={css.waterItemBtn}>
-    <Icon iconId="icon-edit-2" className={css.waterIconBtn} />
+        <Icon iconId="icon-edit-2" className={css.waterIconBtn} />
         </button>
         <button type="button" className={css.waterItemBtn}>
-
+   <Icon iconId="icon-trash-04" className={css.waterIconBtn} />
         </button>
       </div>
     </div>
+
   );
 };
 
