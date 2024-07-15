46af14c85d60c44efc7cc45a55db4ebfcd2ef366
Oleksiy Zhuravlev <zhouravlevav@gmail.com>
2024-07-15 12:40:32 +0300

    name input validation

--- src/components/modal/serviceUserSettingsForm.js
+++ src/components/modal/serviceUserSettingsForm.js
@@ -5,6 +5,10 @@ export const yupValidationSchema = yup.object().shape({
     .string()
     .min(3, 'Must be at least 3 characters')
     .max(40, 'Must be max 40 characters or less')
+    .matches(
+      /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/,
+      'Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer'
+    )
     .required('Username is required'),
   email: yup
     .string()
