35b2e1f7ac9aaa4a6b8a206c4a65910a70e8e57a
Kateryna1812 <super.katuysha2013@gmail.com>
2024-07-15 14:24:26 +0300

    update-deletemodal

--- src/components/modal/DeleteWaterModal/DeleteWaterModal.jsx
+++ src/components/modal/DeleteWaterModal/DeleteWaterModal.jsx
@@ -20,18 +20,16 @@ const DeleteWaterModal = () => {
   };
 
   return (
-    <Modal onClose={handleCancel}>
-      <div className="modal-content">
-        <h2 className="title">Delete entry</h2>
-        <p>Are you sure you want to delete the entry?</p>
-        <Button className="button" type="button" onClick={handleDelete}>
-          Delete
-        </Button>
-        <Button className="button" type="button" onClick={handleCancel}>
-          Cancel
-        </Button>
-      </div>
-    </Modal>
+    <div className="modal-content">
+      <h2 className="title">Delete entry</h2>
+      <p>Are you sure you want to delete the entry?</p>
+      <Button className="button" type="button" onClick={handleDelete}>
+        Delete
+      </Button>
+      <Button className="button" type="button" onClick={handleCancel}>
+        Cancel
+      </Button>
+    </div>
   );
 };
 
