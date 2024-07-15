73cd4de9fd762c08785c7a140e0b4750a90b89fb
Serhii Krasovskyi <chokorik@gmail.com>
2024-07-15 22:25:05 +0300

    cors error

---
+++ src/redux/auth/operations.js
@@ -0,0 +1,71 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: 'https://aquatracker-node.onrender.com',
+});
+
+const setToken = token => {
+  instance.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
+
+const clearToken = () => {
+  instance.defaults.headers.common.Authorization = '';
+};
+
+export const register = createAsyncThunk(
+  'auth/register',
+  async (credentials, thunkAPI) => {
+    try {
+      await instance.post('/users/register', credentials);
+      const response = await instance.post('/users/login', credentials);
+      setToken(response.data.data.accessToken);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const logIn = createAsyncThunk(
+  'auth/login',
+  async (credentials, thunkAPI) => {
+    try {
+      const response = await instance.post('/users/login', credentials);
+      console.log(credentials);
+      setToken(response.data.data.accessToken);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const logOut = createAsyncThunk('auth/logout', async (_, thunkAPI) => {
+  try {
+    await instance.post('/users/logout');
+    clearToken();
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.message);
+  }
+});
+
+export const refreshUser = createAsyncThunk(
+  'auth/refresh',
+  async (_, thunkAPI) => {
+    const {
+      auth: { token },
+    } = thunkAPI.getState();
+    setToken(token);
+
+    const response = await instance.post('/users/refresh');
+    return response.data;
+  },
+  {
+    condition: (_, { getState }) => {
+      const reduxState = getState();
+      const savedToken = reduxState.auth.token;
+      return savedToken !== null;
+    },
+  }
+);
