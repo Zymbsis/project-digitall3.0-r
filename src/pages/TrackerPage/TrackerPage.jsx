db0b0544c7478a5d9181b09f7a7b3420481afbe9
Zymbsis-fs <Oleksandra-fsdev@gmail.com>
2024-07-16 14:55:29 +0200

    update

--- src/pages/TrackerPage/TrackerPage.jsx
+++ src/pages/TrackerPage/TrackerPage.jsx
@@ -3,13 +3,15 @@ import { Button, Container, Section } from 'shared';
 import { logOut } from '../../redux/auth/operations';
 import { getUser } from '../../redux/user/operations';
 import { selectDailyIntake } from '../../redux/water/selectors';
+import { useModal } from '../../context';
+import DeleteWaterModal from '../../components/modal/DeleteWaterModal/DeleteWaterModal';
 // import css from './TrackerPage.module.css';
 
 const TrackerPage = () => {
   const dispatch = useDispatch();
   const water = useSelector(selectDailyIntake);
   console.log(water);
-
+  const { openModal } = useModal();
   return (
     <Section>
       <Container>Welcome to TrackerPage</Container>
@@ -26,6 +28,13 @@ const TrackerPage = () => {
         }}
       >
         User
+      </Button>
+      <Button
+        onClick={() => {
+          openModal(<DeleteWaterModal id={''} />);
+        }}
+      >
+        Modal
       </Button>
     </Section>
   );
