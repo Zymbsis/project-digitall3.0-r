452a27a8ded365d2797332c1e51cf8a345c06ea3
Zymbsis-fs <Oleksandra-fsdev@gmail.com>
2024-07-16 13:55:02 +0200

    update

--- src/pages/TrackerPage/TrackerPage.jsx
+++ src/pages/TrackerPage/TrackerPage.jsx
@@ -2,15 +2,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Button, Container, Section } from 'shared';
 import { logOut } from '../../redux/auth/operations';
 import { getUser } from '../../redux/user/operations';
-import {
-  addWaterIntake,
-  deleteWaterIntake,
-  getInfoByDay,
-  getInfoByMonth,
-  updateWaterIntake,
-} from '../../redux/water/operations';
 import { selectDailyIntake } from '../../redux/water/selectors';
-import { date } from 'yup';
 // import css from './TrackerPage.module.css';
 
 const TrackerPage = () => {
