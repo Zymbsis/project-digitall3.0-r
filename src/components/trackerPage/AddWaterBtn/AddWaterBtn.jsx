2afd304f93d802cc29121f6b4fbed4fd46fd1648
AlbinaKost <143324469+AlbinaKost@users.noreply.github.com>
2024-07-16 16:12:35 +0300

    upd

--- src/components/trackerPage/AddWaterBtn/AddWaterBtn.jsx
+++ src/components/trackerPage/AddWaterBtn/AddWaterBtn.jsx
@@ -19,3 +19,4 @@ const AddWaterBtn = ({ addWater }) => {
 };
 
 export default AddWaterBtn;
+
--- src/components/trackerPage/DailyInfo/DailyInfo.jsx
+++ src/components/trackerPage/DailyInfo/DailyInfo.jsx
@@ -4,10 +4,12 @@ import AddWaterBtn from '../AddWaterBtn/AddWaterBtn';
 import WaterList from '../WaterList/WaterList';
 import css from './DailyInfo.module.css';
 
+
 const DailyInfo = () => {
   return (
     <div className={css.dailyInfoContainer}>
       <ChooseDate />
+
       <AddWaterBtn />
       <WaterList />
     </div>
