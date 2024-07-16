e1aa9e7c5c18b4267e94ed4e2697fb0a90137455
Zymbsis-fs <Oleksandra-fsdev@gmail.com>
2024-07-16 12:17:52 +0200

    .

--- src/components/sigInPage/SignInForm/SignInForm.jsx
+++ src/components/sigInPage/SignInForm/SignInForm.jsx
@@ -45,7 +45,7 @@ const SignInForm = () => {
 
   return (
     <div className={css.wrapper}>
-      <h2 className={css.title}>Sign in</h2>
+      <h2 className={css.title}>Sign In</h2>
       <form onSubmit={handleSubmit(onSubmit)} className={css.form}>
         <label className={css.labelWrapper}>
           <span className={css.label}>Email: </span>
