8bea9868bce533f4e5ee8b996d6027ba018b5dac
AlbinaKost <143324469+AlbinaKost@users.noreply.github.com>
2024-07-17 12:37:54 +0300

    Update AddWaterBtn.jsx

--- src/components/trackerPage/AddWaterBtn/AddWaterBtn.jsx
+++ src/components/trackerPage/AddWaterBtn/AddWaterBtn.jsx
@@ -1,36 +1,36 @@
 import React, { useState } from 'react';
 import { Icon } from 'shared';
 import css from './AddWaterBtn.module.css';
-
-
-
-const AddWaterBtn = ({ addWater }) => {
-
 import { useModal } from '../../../context';
 import WaterModal from '../../modal/WaterModal/WaterModal';
 
-const AddWaterBtn = ({ addWater }) => {
-  const { openModal } = useModal();
-  const handleClick = () => {
-    openModal(<WaterModal type={'add'} />);
+const AddWaterBtn = () => {
+  const AddWaterBtn = ({ addWater }) => {
+    const { openModal } = useModal();
+    const handleClick = () => {
+      openModal(<WaterModal type={'add'} />);
+    };
+
+    return (
+      <div>
+        <Button onClick={handleClick} className={css.btn} type="button">
+          <div className={css.thumb}>
+            <Icon iconId="icon-plus" className={css.iconstyle} />
+            <span className={css.boldtext}>Add water</span>
+      <div className={css.container}>
+        <button type="button" className={css.addWaterBtn} onClick={handleClick}>
+          <div className={css.iconPlusWrap}>
+            <Icon iconId="icon-plus" className={css.iconPlus} />
+          </div>
+        </Button>
+          Add water
+        </button>
+      </div>
+    );
   };
 
+{/* // const AddWaterBtn = () => { */}
 
-  return (
-    <div className={css.container}>
-      <button type="button" className={css.addWaterBtn} onClick={handleClick}>
-        <div className={css.iconPlusWrap}>
-          <Icon iconId="icon-plus" className={css.iconPlus} />
-        </div>
-        Add water
-      </button>
-    </div>
-  );
-};
-
-// const AddWaterBtn = () => {
-//   const { openModal } = useModal();
-//   const handleClick = () => {
 //     openModal(<WaterModal type={'add'} />);
 //   };
 
