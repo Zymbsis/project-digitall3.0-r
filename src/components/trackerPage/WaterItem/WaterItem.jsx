01f6e127af88bcc993bef78168b0756e5735bb17
dan1delon <143418624+dan1delon@users.noreply.github.com>
2024-07-17 13:14:15 +0300

    upd

--- src/components/trackerPage/WaterItem/WaterItem.jsx
+++ src/components/trackerPage/WaterItem/WaterItem.jsx
@@ -1,20 +1,50 @@
+import { useModal } from 'context';
 import css from './WaterItem.module.css';
 import Icon from 'shared/components/Icon/Icon';
+import WaterModal from 'components/modal/WaterModal/WaterModal';
+import DeleteWaterModal from 'components/modal/DeleteWaterModal/DeleteWaterModal';
 
-const WaterItem = () => {
+const WaterItem = ({ water }) => {
+  const { openModal } = useModal();
+  const handleClickDelete = () => {
+    openModal(<DeleteWaterModal type={'add'} />);
+  };
+  const handleClickEdit = () => {
+    openModal(<WaterModal type={'add'} />);
+  };
+
+  const convertTime = time => {
+    let [hours, minutes] = time.split(':').map(Number);
+    const period = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12 || 12;
+    minutes = minutes < 10 ? `0${minutes}` : minutes;
+    hours = hours < 10 ? `${hours}` : hours;
+    return `${hours}:${minutes} ${period}`;
+  };
+
+  const formattedTime = convertTime(water.time);
+
   return (
     <div className={css.waterItem}>
       <Icon iconId="icon-Vector" className={css.waterIcon} />
       <div className={css.waterItemWrap}>
-        <p className={css.waterItemMl}>250 ml</p>
-        <p className={css.waterItemData}>data</p>
+        <p className={css.waterItemMl}>{water.volume} ml</p>
+        <p className={css.waterItemData}>{formattedTime}</p>
       </div>
       <div className={css.waterItemBtnWrap}>
         <button type="button" className={css.waterItemBtn}>
-          <Icon iconId="icon-edit-2" className={css.waterIconBtn} />
+          <Icon
+            iconId="icon-edit-2"
+            className={css.waterIconBtn}
+            onClick={handleClickEdit}
+          />
         </button>
         <button type="button" className={css.waterItemBtn}>
-          <Icon iconId="icon-trash-04" className={css.waterIconBtn} />
+          <Icon
+            iconId="icon-trash-04"
+            className={css.waterIconBtn}
+            onClick={handleClickDelete}
+          />
         </button>
       </div>
     </div>
--- src/components/trackerPage/WaterList/WaterList.jsx
+++ src/components/trackerPage/WaterList/WaterList.jsx
@@ -1,22 +1,68 @@
 import css from './WaterList.module.css';
 import WaterItem from '../WaterItem/WaterItem';
+// import { useSelector } from 'react-redux';
+// import { selectDailyIntake } from '../../../redux/water/selectors';
 
 const WaterList = () => {
+  // const filteredIntake = useSelector(selectDailyIntake);
+
+  const testFilteredIntake = {
+    status: 200,
+    message: 'Successfully found water portions for selected date.',
+    data: {
+      date: '2024-07-01',
+      portions: [
+        {
+          _id: '65e4decdd286b30065d54af9',
+          time: '12:30:00',
+          date: '2025-03-01',
+          volume: 150,
+          userId: '65e4decdd286b30065d54af1',
+          createdAt: '2024-07-01T18:49:47.392Z',
+          updatedAt: '2024-07-01T18:49:47.392Z',
+        },
+        {
+          _id: '65e4decdd286b30065d54af6',
+          time: '12:45:00',
+          date: '2025-03-01',
+          volume: 250,
+          userId: '65e4decdd286b30065d54af1',
+          createdAt: '2024-07-01T18:49:47.392Z',
+          updatedAt: '2024-07-01T18:49:47.392Z',
+        },
+        {
+          _id: '65e4decdd286b30066234af0',
+          time: '9:30:00',
+          date: '2025-03-01',
+          volume: 350,
+          userId: '65e4decdd286b30065d54af1',
+          createdAt: '2024-07-01T18:49:47.392Z',
+          updatedAt: '2024-07-01T18:49:47.392Z',
+        },
+      ],
+      completionRate: 0.65,
+    },
+  };
+
   return (
     <div className={css.waterListWrap}>
       <ul className={css.waterList}>
-        <li className={css.waterItem}>
-          <WaterItem />
-        </li>
-        <li className={css.waterItem}>
-          <WaterItem />
-        </li>
-        <li className={css.waterItem}>
-          <WaterItem />
-        </li>
-        <li className={css.waterItem}>
-          <WaterItem />
-        </li>
+        {Array.isArray(testFilteredIntake.data.portions) &&
+          testFilteredIntake.data.portions.length === 0 && (
+            <li>
+              <p className={css.paragraph}>
+                There are no daily water intakes here yet.
+              </p>
+            </li>
+          )}
+        {Array.isArray(testFilteredIntake.data.portions) &&
+          testFilteredIntake.data.portions.map(water => {
+            return (
+              <li key={water.id} className={css.waterItem}>
+                <WaterItem water={water} />
+              </li>
+            );
+          })}
       </ul>
     </div>
   );
