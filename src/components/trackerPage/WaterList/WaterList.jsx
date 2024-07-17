a3dcb5374d8f325ec5d15a552cf568db67e62806
Zymbsis-fs <Oleksandra-fsdev@gmail.com>
2024-07-17 22:31:02 +0200

    .

--- src/components/trackerPage/WaterList/WaterList.jsx
+++ src/components/trackerPage/WaterList/WaterList.jsx
@@ -11,6 +11,7 @@ import {
   getInfoBySelectedDay,
   getInfoByToday,
 } from '../../../redux/water/operations';
+import { TODAY } from '../../../constants';
 
 const WaterList = () => {
   const dispatch = useDispatch();
@@ -19,14 +20,14 @@ const WaterList = () => {
   const { portions: selectedDayPortions } = useSelector(
     selectInfoBySelectedDay
   );
-  const today = new Date().toISOString().split('T')[0];
-  const condition = selectedDate === null || selectedDate === today;
+  const condition = selectedDate === null || selectedDate === TODAY;
 
   useEffect(() => {
     condition
-      ? dispatch(getInfoByToday(today))
+      ? dispatch(getInfoByToday(TODAY))
       : dispatch(getInfoBySelectedDay(selectedDate));
-  }, [condition, dispatch, today, selectedDate]);
+  }, [condition, dispatch, selectedDate]);
+
   const waterList = condition ? todayPortions : selectedDayPortions;
 
   return (
