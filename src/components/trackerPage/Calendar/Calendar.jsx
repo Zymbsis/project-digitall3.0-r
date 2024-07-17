77375b66eca61fa876f13ab5498b48a2d60db0b2
naowmi <vadimpelyushenko@gmail.com>
2024-07-17 18:59:30 +0200

    Calendar + CalendarItem + style + MonthInfo

--- src/components/trackerPage/Calendar/Calendar.jsx
+++ src/components/trackerPage/Calendar/Calendar.jsx
@@ -1,7 +1,47 @@
-// import css from './Calendar.module.css'
+import css from './Calendar.module.css'
+import CalendarItem from '../CalendarItem/CalendarItem';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectMonthlyStats } from '../../../redux/water/selectors';
+import { getInfoByMonth } from "../../../redux/water/operations";
+import { selectIsLoggedIn } from '../../../redux/auth/selectors';
+import { logIn  } from "../../../redux/auth/operations";
+const Calendar = ({ selectedDate }) => {
+  const [waterData, setWaterData] = useState([]);
+  const dispatch = useDispatch();
+  // const waterData = useSelector(selectMonthlyStats);
+  const isLoggedIn = useSelector(selectIsLoggedIn);
 
-const Calendar = () => {
-  return <div></div>;
+  // useEffect(() => {
+  //   if (isLoggedIn) {
+  //     dispatch(getInfoByMonth(`${selectedDate.getFullYear()}-${selectedDate.getMonth() + 1}`));
+  //   }
+  // }, [selectedDate, isLoggedIn, dispatch]);
+
+  useEffect(() => {
+    const fetchWaterData = async () => {
+      try {
+        const res = await fetch(`/water/month/${selectedDate.getFullYear()}-${selectedDate.getMonth() + 1}`)
+        const data = await res.json();
+        setWaterData(data.days)
+      } catch (error) {
+        console.error(error)
+      }
+    };
+    fetchWaterData();
+  }, [selectedDate]);
+
+  
+  const daysInMonth = new Date(selectedDate.getFullYear(), selectedDate.getMonth() + 1, 0).getDate();
+  return (
+    <ul className={css.container}>
+    {Array.from({ length: daysInMonth }, (_, i) => {
+      const day = i + 1;
+      const dayData = waterData.find(water => water.day === String(day)) || { completionRate: 0};
+      return <CalendarItem key={day} day={day} waterData={dayData} selectedDate={selectedDate} />;
+    })}
+  </ul>
+)
 };
 
 export default Calendar;
--- src/components/trackerPage/CalendarItem/CalendarItem.jsx
+++ src/components/trackerPage/CalendarItem/CalendarItem.jsx
@@ -1,7 +1,39 @@
-// import css from './CalendarItem.module.css'
+import css from './CalendarItem.module.css'
+import clsx from 'clsx';
+import { getInfoByDay } from "../../../redux/water/operations"
+import { Button } from 'shared';
+import { useDispatch } from 'react-redux';
 
-const CalendarItem = () => {
-  return <div></div>;
+
+const CalendarItem = ({ day, selectedDate, waterData}) => {
+
+  const { completionRate } = waterData;
+  const dispatch = useDispatch()
+
+  console.log("CalendarItem props:", { day, selectedDate, waterData });
+
+  const handleWaterOfDay = async (day) => {
+    try {
+      // if (!selectedDate) {
+      //   console.error("selectedDate is undefined");
+      //   return;
+      // }
+
+      const date = `${selectedDate.getFullYear()}-${(selectedDate.getMonth() + 1).toString().padStart(2, '0')}-${String(day).padStart(2, '0')}`;
+      const res = await dispatch(getInfoByDay(date)).unwrap();
+    } catch (error) {
+      console.error("Error fetching water data:", error);
+    }
+  };
+
+  return (
+  <li>
+    <Button onClick={() => handleWaterOfDay(day)} className={clsx(css.button, { [css.completed]: completionRate === 1 })}>
+      <span className={css.day}>{day}</span>
+    </Button>
+    <span className={css.percent}>{Math.round(completionRate * 100)}%</span>
+  </li>
+  );
 };
 
 export default CalendarItem;
