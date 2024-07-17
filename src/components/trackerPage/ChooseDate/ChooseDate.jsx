05c2f914dab65ca0037982c975fae7ec3b7f6f3f
Zymbsis-fs <Oleksandra-fsdev@gmail.com>
2024-07-17 11:25:25 +0200

    edited ChooseDay component

--- src/components/trackerPage/ChooseDate/ChooseDate.jsx
+++ src/components/trackerPage/ChooseDate/ChooseDate.jsx
@@ -1,7 +1,17 @@
+// import { useSelector } from 'react-redux';
 import css from './ChooseDate.module.css';
+// import { selectSelectedDate } from '../../../redux/water/selectors';
 
 const ChooseDate = () => {
-  return <h3 className={css.chooseDateTitle}>Today</h3>;
+  // const selectedDate = useSelector(selectSelectedDate);
+  const example = '2024-17-07';
+  const [year, month, day] = example.split('-');
+  const queryDate = new Date(year, month - 1, day);
+  const monthName = queryDate.toLocaleString('en-US', { month: 'long' });
+  const dayOfMonth = queryDate.getDate();
+  const date = `${dayOfMonth}, ${monthName}`;
+
+  return <h3 className={css.chooseDateTitle}>{example ? date : 'Today'}</h3>;
 };
 
 export default ChooseDate;
