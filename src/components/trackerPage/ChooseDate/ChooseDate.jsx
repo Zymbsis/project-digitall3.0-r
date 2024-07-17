96531add1bd9e47717f380b8a1a8b3e20d9e5f76
dan1delon <143418624+dan1delon@users.noreply.github.com>
2024-07-17 14:03:58 +0300

    Update ChooseDate.jsx

--- src/components/trackerPage/ChooseDate/ChooseDate.jsx
+++ src/components/trackerPage/ChooseDate/ChooseDate.jsx
@@ -1,6 +1,15 @@
 import css from './ChooseDate.module.css';
 
 const ChooseDate = () => {
+  // const formatDate = dateString => {
+  //   const options = { day: 'numeric', month: 'long' };
+  //   const date = new Date(dateString);
+  //   return date.toLocaleString('en-US', options);
+  // };
+
+  // const formattedDate = formatDate('2025-03-01');
+  // console.log(formattedDate);
+
   return <h3 className={css.chooseDateTitle}>Today</h3>;
 };
 
