5605954265a266f836a7dae989e9576081cce91e
AlbinaKost <143324469+AlbinaKost@users.noreply.github.com>
2024-07-17 12:25:00 +0300

    upd

--- src/components/trackerPage/AddWaterBtn/AddWaterBtn.jsx
+++ src/components/trackerPage/AddWaterBtn/AddWaterBtn.jsx
@@ -3,18 +3,7 @@ import { Icon } from 'shared';
 import css from './AddWaterBtn.module.css';
 // import Modal from 'components/Modal/Modal.jsx';
 
-
 const AddWaterBtn = ({ addWater }) => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const handleClick = () => {
-    setIsModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
-
   return (
     <div className={css.container}>
       <button type="button" className={css.addWaterBtn} onClick={handleClick}>
@@ -28,4 +17,3 @@ const AddWaterBtn = ({ addWater }) => {
 };
 
 export default AddWaterBtn;
-
--- src/components/trackerPage/ChooseDate/ChooseDate.jsx
+++ src/components/trackerPage/ChooseDate/ChooseDate.jsx
@@ -1,10 +1,7 @@
-import React, { useState } from 'react';
+
 import css from './ChooseDate.module.css';
 
 const ChooseDate = () => {
-  const today = new Date().getDate();
-  const month = new Date().toLocaleString('en-US', { month: 'long' });
-
 
   return(
   <h3 className={css.chooseDateTitle}>Today</h3>
