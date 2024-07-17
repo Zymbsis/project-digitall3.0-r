8c18664d180ad0db1eec81803d941030c4f29d38
DadaBabii <107479813+DadaBabii@users.noreply.github.com>
2024-07-17 15:32:09 +0300

    fixed %, fixed slider thumb

--- src/components/trackerPage/WaterProgressBar/WaterProgressBar.jsx
+++ src/components/trackerPage/WaterProgressBar/WaterProgressBar.jsx
@@ -1,48 +1,53 @@
+import { useDispatch, useSelector } from 'react-redux';
 import css from './WaterProgressBar.module.css';
 import Slider from '@mui/material/Slider';
+import { useEffect } from 'react';
+import { getInfoByDay } from '../../../redux/water/operations.js';
+import { selectDailyIntake } from '../../../redux/water/selectors.js';
 
 const WaterProgressBar = () => {
-  // const dispatch = useDispatch();
-  // const { completionRate } = useSelector(selectDailyIntake);
-  // const today = new Date().toISOString().split('T')[0];
-  // const percentOfWater = Math.round(completionRate * 100);
+  const dispatch = useDispatch();
+  const { completionRate } = useSelector(selectDailyIntake);
+  const today = new Date().toISOString().split('T')[0];
+  const percentOfWater = Math.round(completionRate * 100);
   // const visibleMarkWater = [0, 50, 100];
+  const marks = [
+    {
+      value: percentOfWater,
+      label: `${percentOfWater}%`,
+    },
+  ];
+
+  useEffect(() => {
+    dispatch(getInfoByDay(today));
+  }, [dispatch, today]);
+
+  const day = 'Today';
+  // const percentOfWater = 23;
+  // const invisMarkWater = [0, 50, 100];
+
   // const marks = [
   //   {
   //     value: percentOfWater,
   //     label: `${percentOfWater}%`,
+  //     // label: `${percentOfWater}`,
   //   },
   // ];
-
-  // useEffect(() => {
-  //   dispatch(getInfoByDay(today));
-  // }, [dispatch, today]);
-
-  const day = 'Today';
-  const persentOfWater = 75;
-  const invisMarkWater = [0, 50, 100];
-
-  const marks = [
-    {
-      value: persentOfWater,
-      label: `${persentOfWater}%`,
-    },
-  ];
 
   return (
     <div className={css.thumb}>
       <p className={css.boldtext}>{day}</p>
       <Slider
         className={css.slider}
-        defaultValue={persentOfWater}
+        value={percentOfWater}
         marks={marks}
-        sx={{
-          '& .MuiSlider-markLabel': {
-            visibility: invisMarkWater.includes(persentOfWater)
-              ? 'hidden'
-              : 'visible',
-          },
-        }}
+        // sx={{
+        //   '& .MuiSlider-markLabel': {
+        //     visibility: invisMarkWater.includes(persentOfWater)
+        //       ? 'hidden'
+        //       : 'visible',
+        //   },
+        // }}
       />
       <ul className={css.scale}>
         <li className={css.normaltext}>0%</li>
