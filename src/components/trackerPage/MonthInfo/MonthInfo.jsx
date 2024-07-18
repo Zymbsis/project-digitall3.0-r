f2d9b21f91e28fbdfe5868dced533116b43af969
Zymbsis-fs <Oleksandra-fsdev@gmail.com>
2024-07-18 22:13:46 +0200

    .

--- src/components/trackerPage/MonthInfo/MonthInfo.jsx
+++ src/components/trackerPage/MonthInfo/MonthInfo.jsx
@@ -1,7 +1,15 @@
 // import css from './MonthInfo.module.css';
 
+import Calendar from '../Calendar/Calendar';
+import CalendarPagination from '../CalendarPagination/CalendarPagination';
+
 const MonthInfo = () => {
-  return <div></div>;
+  return (
+    <div>
+      <CalendarPagination />
+      <Calendar />
+    </div>
+  );
 };
 
 export default MonthInfo;
