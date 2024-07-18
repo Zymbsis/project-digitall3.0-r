68bf97fbc1c6d3e1c82356499578575abbd724fa
Zymbsis-fs <Oleksandra-fsdev@gmail.com>
2024-07-18 16:55:47 +0200

    update

--- src/pages/TrackerPage/TrackerPage.jsx
+++ src/pages/TrackerPage/TrackerPage.jsx
@@ -1,46 +1,18 @@
-import { useDispatch } from 'react-redux';
-import { Button, Container, Section } from 'shared';
-import { getUser } from '../../redux/user/operations';
-import { useModal } from '../../context';
-// import DeleteWaterModal from '../../components/modal/DeleteWaterModal/DeleteWaterModal';
-import LogOutModal from '../../components/modal/LogOutModal/LogOutModal';
-import css from './TrackerPage.module.css';
-import { WaterDetailedInfo, WaterMainInfo } from 'components';
 import { useEffect } from 'react';
-import UserSettingsForm from '../../components/modal/UserSettingsForm/UserSettingsForm';
+import { useDispatch } from 'react-redux';
+import { Container, Section } from 'shared';
+import { getUser } from '../../redux/user/operations';
+import { WaterDetailedInfo, WaterMainInfo } from 'components';
+import css from './TrackerPage.module.css';
 
 const TrackerPage = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getUser());
   }, [dispatch]);
-  const { openModal } = useModal();
 
   return (
     <Section>
-      {/* <Button
-          onClick={() => {
-            dispatch(logOut());
-          }}
-        >
-          Logout
-        </Button> */}
-
-      <Button
-        onClick={() => {
-          openModal(<LogOutModal />);
-        }}
-      >
-        Logout
-      </Button>
-
-      <Button
-        onClick={() => {
-          openModal(<UserSettingsForm />);
-        }}
-      >
-        UserSettings
-      </Button>
       <Container className={css.container}>
         <WaterMainInfo />
         <WaterDetailedInfo />
