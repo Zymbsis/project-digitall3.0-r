8e73e2edcb437e4fdeb00dd212b1780596f979e6
Zymbsis-fs <Oleksandra-fsdev@gmail.com>
2024-07-19 00:35:35 +0200

    .

--- src/validationSchemas/userSettingsFormSchema.js
+++ src/validationSchemas/userSettingsFormSchema.js
@@ -12,10 +12,7 @@ export const userSettingsFormSchema = yup.object().shape({
   email: yup
     .string()
     .max(40, 'Must be max 40 characters or less')
-    .matches(
-      /^[\w.%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-      'Enter a valid email address, for example "nJ8pN@example.com".'
-    )
+    .email()
     .required('Email is required'),
   weight: yup.number().max(150, 'Must not be more then 150 kg'),
   activeHours: yup.number().max(12, 'Must not be more then 12 hours'),
