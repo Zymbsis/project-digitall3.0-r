3413b6417ef78ba0be6cc3d015cf5ad452b06607
olena-semenenko <elen.kinakh@gmail.com>
2024-07-21 01:04:04 +0200

    Update DailyInfo.jsx

--- src/components/trackerPage/DailyInfo/DailyInfo.jsx
+++ src/components/trackerPage/DailyInfo/DailyInfo.jsx
@@ -2,10 +2,11 @@ import ChooseDate from '../ChooseDate/ChooseDate';
 import AddWaterBtn from '../AddWaterBtn/AddWaterBtn';
 import WaterList from '../WaterList/WaterList';
 import css from './DailyInfo.module.css';
+import clsx from 'clsx';
 
 const DailyInfo = () => {
   return (
-    <div className={css.dailyInfoContainer}>
+    <div className={(clsx(css.dailyInfoContainer), 'tour-daily-progress')}>
       <div className={css.wrapperTop}>
         <ChooseDate />
         <AddWaterBtn className="dailyInfoStyles" />
