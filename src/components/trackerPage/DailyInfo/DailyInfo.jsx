5a7782e20cf07d6fcdb9bbd5a5a9b5d23941635c
Zymbsis-fs <Oleksandra-fsdev@gmail.com>
2024-07-20 17:26:44 +0200

    .

--- src/components/trackerPage/DailyInfo/DailyInfo.jsx
+++ src/components/trackerPage/DailyInfo/DailyInfo.jsx
@@ -2,13 +2,17 @@ import ChooseDate from '../ChooseDate/ChooseDate';
 import AddWaterBtn from '../AddWaterBtn/AddWaterBtn';
 import WaterList from '../WaterList/WaterList';
 import css from './DailyInfo.module.css';
+import { useSelector } from 'react-redux';
+import { selectSelectedDate } from '../../../redux/water/selectors';
 
 const DailyInfo = () => {
+  const selectedDate = useSelector(selectSelectedDate);
+
   return (
     <div className={css.dailyInfoContainer}>
       <div className={css.wrapperTop}>
         <ChooseDate />
-        <AddWaterBtn className="dailyInfoStyles" />
+        <AddWaterBtn className="dailyInfoStyles" date={selectedDate} />
       </div>
       <WaterList />
     </div>
@@ -16,4 +20,3 @@ const DailyInfo = () => {
 };
 
 export default DailyInfo;
-
