f32a7d3aafa42c5e1f1e481a378ee34ed4852cd4
Zymbsis-fs <Oleksandra-fsdev@gmail.com>
2024-07-20 22:58:14 +0200

    C:/Program Files/Git/

--- src/components/homePage/AdvantagesSection/Advantages/Advantages.jsx
+++ src/components/homePage/AdvantagesSection/Advantages/Advantages.jsx
@@ -2,18 +2,18 @@ import css from './Advantages.module.css';
 
 const Advantages = () => {
   return (
-    <div className={css.advantages}>
-      <div className={css.habit}>
+    <ul className={css.advantages}>
+      <li className={css.habit}>
         <div className={css.dot}></div>
         <p className={css.habitText}>Habit drive</p>
-      </div>
-      <div className={css.statistics}>
+      </li>
+      <li className={css.statistics}>
         <p className={css.statisticsText}>View statistics</p>
-      </div>
-      <div className={css.rate}>
+      </li>
+      <li className={css.rate}>
         <p className={css.rateText}>Personal rate setting</p>
-      </div>
-    </div>
+      </li>
+    </ul>
   );
 };
 
