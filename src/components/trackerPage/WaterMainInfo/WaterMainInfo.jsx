de75106f63ee9a4d4a5e8582238e15c0678a32aa
Zymbsis-fs <Oleksandra-fsdev@gmail.com>
2024-07-20 22:58:38 +0200

    .

--- src/components/trackerPage/WaterMainInfo/WaterMainInfo.jsx
+++ src/components/trackerPage/WaterMainInfo/WaterMainInfo.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { getInfoByDay } from '../../../redux/water/operations';
 import { parseDayForFetch } from 'helpers';
 import AddWaterBtn from '../AddWaterBtn/AddWaterBtn';
