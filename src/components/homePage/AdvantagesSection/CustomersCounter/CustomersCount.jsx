39a259f61b8bdb692ada79ae35956ffc419548fc
Zymbsis-fs <Oleksandra-fsdev@gmail.com>
2024-07-21 02:06:01 +0200

    .

--- src/components/homePage/AdvantagesSection/CustomersCounter/CustomersCount.jsx
+++ src/components/homePage/AdvantagesSection/CustomersCounter/CustomersCount.jsx
@@ -1,5 +1,7 @@
 import { useSelector } from 'react-redux';
 import { selectCountUser } from '../../../../redux/user/selectors.js';
+// import blondeWoman from 'image';
+// import photo from 'image/index.js';
 import css from './CustomersCount.module.css';
 const CustomersCounter = () => {
   const countedUsers = useSelector(selectCountUser);
@@ -8,10 +10,11 @@ const CustomersCounter = () => {
     <div className={css.customers}>
       <ul className={css.customersPhotos}>
         <li>
+          {/* <picture><source srcset=${pathToPhotoWebP} type="image/webp"/><img class="team-modal-img" loading="lazy" src=${pathToPhotoPNG} width="320" height="320" alt=${description} type="image/png"/></picture> */}
           <img
             className={css.customersPhotoOne}
             // srcSet="../img/Customers@1x-1.webp 1x, ../img/Customers@1x-1.png 1x, ../img/Customers@2x-1.webp 2x, ../img/Customers@2x-1.png 2x"
-            src="img/customers/Customers@1x-1.png"
+            src={''}
             alt="blonde woman"
           />
         </li>
@@ -19,7 +22,7 @@ const CustomersCounter = () => {
           <img
             className={css.customersPhotoTwo}
             // srcSet="../img/Customers@1x-2.webp 1x, ../img/Customers@1x-2.png 1x, ../img/Customers@2x-2.webp 2x, ../img/Customers@2x-2.png 2x"
-            src="customers/Customers@1x-1.png"
+            src={''}
             alt="young man"
           />
         </li>
@@ -27,7 +30,7 @@ const CustomersCounter = () => {
           <img
             className={css.customersPhotoThree}
             // srcSet="../img/Customers@1x-3.webp 1x, ../img/Customers@1x-3.png 1x, ../img/Customers@2x-3.webp 2x, ../img/Customers@2x-3.png 2x"
-            src="assets/customers/Customers@1x-3.webp"
+            src={''}
             alt="young woman"
           />
         </li>
---
+++ src/image/index.js
@@ -0,0 +1,1 @@
+export { default as icon } from './customers/Customers@1x-1.png';
