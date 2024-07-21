a82a836100a1e82437def253662c8f70197e98b1
Zymbsis-fs <Oleksandra-fsdev@gmail.com>
2024-07-21 03:16:38 +0200

    custoners

--- src/components/homePage/AdvantagesSection/CustomersCounter/CustomersCount.jsx
+++ src/components/homePage/AdvantagesSection/CustomersCounter/CustomersCount.jsx
@@ -1,8 +1,8 @@
 import { useSelector } from 'react-redux';
 import { selectCountUser } from '../../../../redux/user/selectors.js';
-// import blondeWoman from 'image';
-// import photo from 'image/index.js';
+import { images } from 'image';
 import css from './CustomersCount.module.css';
+
 const CustomersCounter = () => {
   const countedUsers = useSelector(selectCountUser);
 
@@ -10,27 +10,41 @@ const CustomersCounter = () => {
     <div className={css.customers}>
       <ul className={css.customersPhotos}>
         <li>
-          {/* <picture><source srcset=${pathToPhotoWebP} type="image/webp"/><img class="team-modal-img" loading="lazy" src=${pathToPhotoPNG} width="320" height="320" alt=${description} type="image/png"/></picture> */}
           <img
             className={css.customersPhotoOne}
-            // srcSet="../img/Customers@1x-1.webp 1x, ../img/Customers@1x-1.png 1x, ../img/Customers@2x-1.webp 2x, ../img/Customers@2x-1.png 2x"
-            src={''}
+            srcSet={`
+      ${images.customers_1x_1_webp} 1x,
+      ${images.customers_1x_1_png} 1x,
+      ${images.customers_2x_1_webp} 2x,
+      ${images.customers_2x_1_png} 2x
+    `}
+            src={images.customers_1x_1_png}
             alt="blonde woman"
           />
         </li>
         <li>
           <img
             className={css.customersPhotoTwo}
-            // srcSet="../img/Customers@1x-2.webp 1x, ../img/Customers@1x-2.png 1x, ../img/Customers@2x-2.webp 2x, ../img/Customers@2x-2.png 2x"
-            src={''}
+            srcSet={`
+      ${images.customers_1x_2_webp} 1x,
+      ${images.customers_1x_2_png} 1x,
+      ${images.customers_2x_2_webp} 2x,
+      ${images.customers_2x_2_png} 2x
+    `}
+            src={images.customers_1x_2_png}
             alt="young man"
           />
         </li>
         <li>
           <img
             className={css.customersPhotoThree}
-            // srcSet="../img/Customers@1x-3.webp 1x, ../img/Customers@1x-3.png 1x, ../img/Customers@2x-3.webp 2x, ../img/Customers@2x-3.png 2x"
-            src={''}
+            srcSet={`
+      ${images.customers_1x_3_webp} 1x,
+      ${images.customers_1x_3_png} 1x,
+      ${images.customers_2x_3_webp} 2x,
+      ${images.customers_2x_3_png} 2x
+    `}
+            src={images.customers_1x_3_png}
             alt="young woman"
           />
         </li>
