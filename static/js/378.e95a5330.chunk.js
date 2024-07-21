5e498c1b92e680e1e814449f916c7cdfde054195
Zymbsis <143128838+Zymbsis@users.noreply.github.com>
2024-07-21 01:17:38 +0000

    Deploying to gh-pages from @ Zymbsis/project-digitall3.0-r@a82a836100a1e82437def253662c8f70197e98b1 🚀

--- static/js/378.e95a5330.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkproject_digitall3_0_r=self.webpackChunkproject_digitall3_0_r||[]).push([[378],{5378:function(e,r,s){s.r(r);var t=s(1553),c=s(5362),i=s(3329);r.default=function(){return(0,i.jsx)(t.$0,{children:(0,i.jsxs)(t.W2,{children:[(0,i.jsx)(t.TR,{}),(0,i.jsx)(c.aI,{}),(0,i.jsx)(c.UH,{})]})})}}}]);
-//# sourceMappingURL=378.e95a5330.chunk.js.map
