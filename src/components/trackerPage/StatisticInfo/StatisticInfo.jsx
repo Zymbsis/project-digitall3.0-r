c364e8ef46b212c139ae4506759aaaec3464884d
DadaBabii <107479813+DadaBabii@users.noreply.github.com>
2024-07-21 18:45:02 +0300

    statisticSection, statisticInfo, weekDiagramm added

--- src/components/trackerPage/StatisticInfo/StatisticInfo.jsx
+++ src/components/trackerPage/StatisticInfo/StatisticInfo.jsx
@@ -1,63 +1,10 @@
 import React from 'react';
 import css from './StatisticInfo.module.css';
 
-import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { selectCurrentUser } from '../../../redux/user/selectors';
-import { getInfoByMonth } from '../../../redux/water/operations';
-import {
-  parseDayForFetch,
-  parseMonthForFetch,
-  parseSelectedMonth,
-} from 'helpers';
+import { parseSelectedMonth } from 'helpers';
 import { Icon } from 'shared';
 
 const StatisticInfo = ({ selectedDate }) => {
-  // const dispatch = useDispatch();
-
-  // const { createdAt } = useSelector(selectCurrentUser);
-  // const userCreatedDate = createdAt ? new Date(createdAt) : new Date();
-  // const limitDate = new Date();
-  // const currentDay = parseDayForFetch(new Date());
-  // limitDate.setFullYear(limitDate.getFullYear() + 1);
-
-  // const hasPrevMonth =
-  //   selectedDate.getFullYear() < userCreatedDate.getFullYear() ||
-  //   (selectedDate.getFullYear() === userCreatedDate.getFullYear() &&
-  //     selectedDate.getMonth() <= userCreatedDate.getMonth());
-
-  // const hasNextMonth =
-  //   selectedDate.getFullYear > limitDate.getFullYear() ||
-  //   (selectedDate.getFullYear() === limitDate.getFullYear() &&
-  //     selectedDate.getMonth() >= limitDate.getMonth());
-
-  // useEffect(() => {
-  //   const dateForFetch = parseMonthForFetch(selectedDate, currentDay);
-  //   dispatch(getInfoByMonth(dateForFetch));
-  // }, [selectedDate, currentDay, dispatch]);
-
-  // const handlePrevMonth = () => {
-  //   setSelectedDate(prevDate => {
-  //     const prevMonthDate = new Date(
-  //       prevDate.getFullYear(),
-  //       prevDate.getMonth() - 1,
-  //       1
-  //     );
-  //     return prevMonthDate;
-  //   });
-  // };
-
-  // const handleNextMonth = () => {
-  //   setSelectedDate(nextDate => {
-  //     const nextMonthDate = new Date(
-  //       nextDate.getFullYear(),
-  //       nextDate.getMonth() + 1,
-  //       1
-  //     );
-  //     return nextMonthDate;
-  //   });
-  // };
-
   return (
     <div className={css.container}>
       <h2 className={css.title}>Statistic</h2>
@@ -67,15 +14,21 @@ const StatisticInfo = ({ selectedDate }) => {
           // onClick={handlePrevMonth}
           // disabled={hasPrevMonth}
         >
-          <Icon iconId="icon-chevron-left" className={css.logo} />
+          <Icon iconId="icon-chevron-down" className={css.logo} />
         </div>
         <p className={css.date}>{parseSelectedMonth(selectedDate)}</p>
-        <button
+        <div
           className={css.iconBtn}
           // onClick={handleNextMonth}
           // disabled={hasNextMonth}
         >
-          <Icon iconId="icon-chevron-right" className={css.logo} />
+          <Icon iconId="icon-chevron-down" className={css.logoleft} />
+        </div>
+        <button
+          className={css.iconBtn}
+          // onClick={handleNextMonth}
+        >
+          <Icon iconId="icon-pie-chart-02" className={css.logopiechart} />
         </button>
       </div>
     </div>
