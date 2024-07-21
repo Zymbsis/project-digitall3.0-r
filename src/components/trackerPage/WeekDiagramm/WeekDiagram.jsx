5b2f556ac4bfa1fef6b28e7b7d6a299af1f58d4b
DadaBabii <107479813+DadaBabii@users.noreply.github.com>
2024-07-21 14:25:31 +0300

    water by today and -7 days is drawn

--- src/components/trackerPage/WeekDiagramm/WeekDiagram.jsx
+++ src/components/trackerPage/WeekDiagramm/WeekDiagram.jsx
@@ -2,16 +2,8 @@ import React, { useEffect, useState } from 'react';
 
 import css from './WeekDiagram.module.css';
 import { AreaChart, XAxis, YAxis, Tooltip, Area } from 'recharts';
-
-const data = [
-  { name: '12', uv: 1200 },
-  { name: '13', uv: 1500 },
-  { name: '14', uv: 700 },
-  { name: '15', uv: 2000 },
-  { name: '16', uv: 1200 },
-  { name: '17', uv: 1000 },
-  { name: '18', uv: 1800 },
-];
+import { useSelector } from 'react-redux';
+import { selectInfoByMonth } from '../../../redux/water/selectors.js';
 
 const CustomTooltip = ({ active, payload }) => {
   if (active && payload && payload.length) {
@@ -33,6 +25,34 @@ const CustomTooltip = ({ active, payload }) => {
 };
 
 const WeekDiagram = () => {
+  const getWaterDataForLast7Days = waterData => {
+    const today = new Date();
+    let daysArray = [];
+
+    for (let i = 6; i >= 0; i--) {
+      const date = new Date();
+      date.setDate(today.getDate() - i);
+      const day = date.getDate().toString().padStart(2, '0');
+
+      const dayData = waterData.find(water => water.day === day) || {
+        portions: [],
+      };
+      console.log(waterData);
+      const volume = dayData.portions.reduce(
+        (total, portion) => total + portion.volume,
+        0
+      );
+      daysArray.push({ name: day, uv: volume });
+    }
+
+    return daysArray;
+  };
+
+  const { days: waterData } = useSelector(selectInfoByMonth);
+
+  const transformedData = getWaterDataForLast7Days(waterData);
+  console.log(transformedData);
+
   const [chartSize, setChartSize] = useState({
     width: 244,
     height: 256,
@@ -95,7 +115,7 @@ const WeekDiagram = () => {
     if (value === 0) {
       return '0%';
     }
-    return `${value / 1000} L`;
+    return `${(value / 1000).toFixed(1)} L`;
   };
 
   return (
@@ -105,7 +125,7 @@ const WeekDiagram = () => {
         // className={css.areaChartWrapper}
         width={chartSize.width}
         height={chartSize.height}
-        data={data}
+        data={transformedData}
         margin={{ top: 20, right: 0, left: 0, bottom: 20 }}
       >
         <defs>
@@ -202,3 +222,131 @@ export default WeekDiagram;
 //   stroke: 'var( --hover-green-color)',
 // }}
 // />
+
+// const transformonmData = [
+//   { name: '12', uv: 1200 },
+//   { name: '13', uv: 1500 },
+//   { name: '14', uv: 700 },
+//   { name: '15', uv: 2000 },
+//   { name: '16', uv: 1200 },
+//   { name: '17', uv: 1000 },
+//   { name: '18', uv: 1800 },
+// ];
+
+// const monthInfo = {
+//   date: '2024-07',
+//   days: [
+//     {
+//       portions: [
+//         {
+//           _id: '669bb542dde1070bf232c7ff',
+//           userId: '6696da096b946c399dd89cf2',
+//           time: '09:00:05',
+//           date: '2024-07-01',
+//           volume: 1000,
+//           dailyNorma: 1500,
+//           createdAt: '2024-07-20T13:01:54.929Z',
+//           updatedAt: '2024-07-20T13:01:54.929Z',
+//         },
+//       ],
+//       completionRate: 0.6666666666666666,
+//       day: '01',
+//     },
+//     {
+//       portions: [
+//         {
+//           _id: '669bb555dde1070bf232c88d',
+//           userId: '6696da096b946c399dd89cf2',
+//           time: '09:00:05',
+//           date: '2024-07-02',
+//           volume: 900,
+//           dailyNorma: 1500,
+//           createdAt: '2024-07-20T13:02:13.337Z',
+//           updatedAt: '2024-07-20T13:02:13.337Z',
+//         },
+//       ],
+//       completionRate: 0.6,
+//       day: '02',
+//     },
+//     {
+//       portions: [
+//         {
+//           _id: '669bb563dde1070bf232c9a8',
+//           userId: '6696da096b946c399dd89cf2',
+//           time: '09:00:05',
+//           date: '2024-07-03',
+//           volume: 870,
+//           dailyNorma: 1500,
+//           createdAt: '2024-07-20T13:02:27.903Z',
+//           updatedAt: '2024-07-20T13:02:27.903Z',
+//         },
+//       ],
+//       completionRate: 0.58,
+//       day: '03',
+//     },
+//     {
+//       portions: [
+//         {
+//           _id: '669bb570dde1070bf232c9ac',
+//           userId: '6696da096b946c399dd89cf2',
+//           time: '09:00:05',
+//           date: '2024-07-04',
+//           volume: 1000,
+//           dailyNorma: 1500,
+//           createdAt: '2024-07-20T13:02:40.602Z',
+//           updatedAt: '2024-07-20T13:02:40.602Z',
+//         },
+//       ],
+//       completionRate: 0.6666666666666666,
+//       day: '04',
+//     },
+//     {
+//       portions: [
+//         {
+//           _id: '669bb57ddde1070bf232c9b0',
+//           userId: '6696da096b946c399dd89cf2',
+//           time: '09:00:05',
+//           date: '2024-07-05',
+//           volume: 900,
+//           dailyNorma: 1500,
+//           createdAt: '2024-07-20T13:02:53.993Z',
+//           updatedAt: '2024-07-20T13:02:53.993Z',
+//         },
+//       ],
+//       completionRate: 0.6,
+//       day: '05',
+//     },
+//     {
+//       portions: [
+//         {
+//           _id: '669bb58cdde1070bf232c9b4',
+//           userId: '6696da096b946c399dd89cf2',
+//           time: '09:00:05',
+//           date: '2024-07-06',
+//           volume: 700,
+//           dailyNorma: 1500,
+//           createdAt: '2024-07-20T13:03:08.379Z',
+//           updatedAt: '2024-07-20T13:03:08.379Z',
+//         },
+//       ],
+//       completionRate: 0.4666666666666667,
+//       day: '06',
+//     },
+//     {
+//       portions: [
+//         {
+//           _id: '669bb59adde1070bf232c9b8',
+//           userId: '6696da096b946c399dd89cf2',
+//           time: '09:00:05',
+//           date: '2024-07-07',
+//           volume: 800,
+//           dailyNorma: 1500,
+//           createdAt: '2024-07-20T13:03:22.318Z',
+//           updatedAt: '2024-07-20T13:03:22.318Z',
+//         },
+//       ],
+//       completionRate: 0.5333333333333333,
+//       day: '07',
+//     },
+//   ],
+// };
