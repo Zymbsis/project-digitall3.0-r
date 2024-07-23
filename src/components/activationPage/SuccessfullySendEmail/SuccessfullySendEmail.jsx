9c702036bc99b3e1cd0bd7552f5b53afe6b0d86e
dan1delon <143418624+dan1delon@users.noreply.github.com>
2024-07-23 14:41:40 +0300

    upd

--- src/components/activationPage/SuccessfullySendEmail/SuccessfullySendEmail.jsx
+++ src/components/activationPage/SuccessfullySendEmail/SuccessfullySendEmail.jsx
@@ -1,11 +1,22 @@
+import { Button, Title } from 'shared';
 import css from './SuccessfullySendEmail.module.css';
+import { useModal } from 'context';
 
 const SuccessfullySendEmail = ({ email }) => {
+  const { closeModal } = useModal();
+  const handleClose = e => {
+    closeModal(e);
+  };
+
   return (
     <div className={css.wrapper}>
+      <Title className={css.title}>Email Verification</Title>
       <p className={css.text}>
-        An activation email has been successfully sent to your inbox{' '}
+        Please verify your email address by clicking the link sent to:{' '}
         <span>{email}</span>
+        <Button className={css.closeBtn} onClick={handleClose}>
+          Close
+        </Button>
       </p>
     </div>
   );
